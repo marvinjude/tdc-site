c64c9a4e3f07a58aac2114701fe9220c5e884e1f
Agboola Jude <marvinjudehk@yahoo.com>
2018-12-16 18:30:16 +0100

    removed typographyjs

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,12 +15,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `./src/utils/typography`,
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
