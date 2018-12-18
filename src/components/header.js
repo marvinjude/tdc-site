04e85dead0ee5bec8d1e8c55da6ab39ec37657e2
Agboola Jude <marvinjudehk@yahoo.com>
2018-12-18 09:49:08 +0100

    added registration link

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,16 @@ import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Header = ({ siteTitle }) => <div className="header">TDC</div>
+const Header = ({ siteTitle }) => (
+  <div className="header">
+    TDC
+    <div className="links">
+      <a className="btn" role="button" href="http://bit.ly/tdc-meetup">
+        {' '}
+        REGISTER
+      </a>
+    </div>
+  </div>
+)
 
 export default Header
